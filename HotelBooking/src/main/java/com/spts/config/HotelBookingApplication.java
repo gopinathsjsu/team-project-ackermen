340c11b10860233ccaaafd58342b024ace307298
Teja Ganapati Jaddipal(SJSU ID- 015957526)) <teja.jaddipal@gmail.com>
2022-04-28 19:55:21 -0700

    Controller to get all bookings by a specific user id
    Also add new package name in scan base packages

--- HotelBooking/src/main/java/com/spts/config/HotelBookingApplication.java
+++ HotelBooking/src/main/java/com/spts/config/HotelBookingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // The packages of rest controllers must be the sub packages of  package of this class or use "scanbasepackages property as below"
-@SpringBootApplication(scanBasePackages = {"com.spts.booking", "com.spts.login","com.spts.search","com.spts.signup","com.spts.admin","com.spts.helper"})
+@SpringBootApplication(scanBasePackages = {"com.spts.booking", "com.spts.login","com.spts.search","com.spts.signup","com.spts.admin","com.spts.helper","com.spts.misc"})
 public class HotelBookingApplication {
 
 	public static void main(String[] args) {
