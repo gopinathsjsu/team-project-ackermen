54db2325e267d6701cfdd0c47737e685758483a8
Teja Ganapati Jaddipal(SJSU ID- 015957526)) <teja.jaddipal@gmail.com>
2022-05-09 00:28:03 -0700

    Check user login
    Check user login for delete and modify booking

--- HotelBooking/src/main/java/com/spts/booking/ModifyReservationController.java
+++ HotelBooking/src/main/java/com/spts/booking/ModifyReservationController.java
@@ -1,5 +1,7 @@
 package com.spts.booking;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -9,22 +11,32 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.spts.signup.User;
+
 
 @RestController
-public class ModifyReservationController {
+public class ModifyReservationController{
 	@Autowired
 	private ModifyReservationImpl modifyReservation;
 	
+	private final String notLoggedIn = "Must be logged in.";
+	
 	@DeleteMapping(value = "/deleteBooking")
-	public String deleteBooking(@RequestBody Booking booking) {
+	public String deleteBooking(@RequestBody Booking booking, HttpServletRequest request) {
+		
+		User user = (User) request.getSession().getAttribute("user");
+        if (user == null) return notLoggedIn;
+		
 		String json="";
 		int code = -2;
 		   ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-		code = modifyReservation.deleteBooking(booking);
+		code = modifyReservation.deleteBooking(booking,user);
 		switch(code) {
 		   case 1111 : json = "Booking cancelled succesfully";
 		   break;
 		   case 2222 : json = "Something went wrong while cancelling your booking";
+		   break;
+		   case 3333 : json = "Permission denied to delete booking!";
 		   break;
 		   case 4444 : json = "No booking record exists with provided booking id, Please check";
 		   break;
@@ -41,11 +53,13 @@ public class ModifyReservationController {
 	}
 	
 	@PutMapping(value = "/changeBooking")
-	public String changeBooking(@RequestBody Booking newBooking) {
+	public String changeBooking(@RequestBody Booking newBooking, HttpServletRequest request) {
+		User user = (User) request.getSession().getAttribute("user");
+        if (user == null) return notLoggedIn;
 		String json="";
 		int code = -2;
 		   ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-		 code  = modifyReservation.changeBooking(newBooking);
+		 code  = modifyReservation.changeBooking(newBooking,user);
 		 switch(code) {
 		 
 		 case 0: json = "Problem in modifying booking, try again";
@@ -60,6 +74,8 @@ public class ModifyReservationController {
 	     break;
 	     case 5555 : json = "Invalid user is, Please check";
 	     break;
+	     case 6666 : json = "Permission denied to delete booking!";
+		 break;
 		 case 1: json = "Booking modified successfully";
 		 break;
 		 default: json = "Unknown error";
