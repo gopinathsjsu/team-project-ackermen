b194adf36edf41965da3daaa4082cf4a94eb5c77
Saikrishna_Dsk <saikrishnadosapati@gmail.com>
2022-05-12 02:02:10 -0700

    Search Page + Results + Static Resources Update

--- app/hotelSearchResults/hotelSearchResults.js
+++ app/hotelSearchResults/hotelSearchResults.js
@@ -9,27 +9,31 @@ for (let i = 0; i < searchResult.length; i++) {
     let div = document.createElement('div');
 
     div.classList.add('childDiv' + i.toString())
+    //div.classList.add('card-style')
+    div.setAttribute('style','border-style: double;padding: 20px;margin: 20px;border-radius: 15px;')
+
+   // style="border-style: double;padding: 20px;margin: 20px;border-radius: 15px;"
 
     let img = document.createElement('img');
-    img.src = "/Users/dsk/Documents/Dsk/Github/team-project-ackermen/app/StaticResources/logo.png"
+    img.src = "../StaticResources/img-"+i+".jpg"
     console.log(img)
-    img.setAttribute('width', '100px');
-    img.setAttribute('height', '100px');
+    img.setAttribute('width', '450px');
+    img.setAttribute('height', '230px');
 
     let h2 = document.createElement('h2');
     h2.textContent = '' + searchResult[i]["hotelName"];
 
     let p1 = document.createElement('p');
-    p1.textContent = 'from $ ' + searchResult[i]["singlePrice"];
+    p1.textContent = 'Starting from : $' + searchResult[i]["singlePrice"] + '/night';
 
 
     let p2 = document.createElement('p');
-    p2.textContent = 'address is: ' + searchResult[i]["address"];
+    p2.textContent = 'Location : ' + searchResult[i]["address"];
 
     let button = document.createElement("button")
     button.setAttribute("type", "submit")
     button.setAttribute("font-size", "15px")
-    button.textContent = "submit"
+    button.textContent = "Select Hotel"
     button.setAttribute('id', "button" + i.toString())
     button.setAttribute('data-hotelid', searchResult[i]["hotelId"].toString())
 
