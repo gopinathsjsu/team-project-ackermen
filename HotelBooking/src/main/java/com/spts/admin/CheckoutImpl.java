f5cc5a0468a00f381a083e9737f3ba887d076a70
slahari5 <sailahariseethamraju@gmail.com>
2022-05-12 13:13:25 -0700

    Implementation of API for user checkout functionality

---
+++ HotelBooking/src/main/java/com/spts/admin/CheckoutImpl.java
@@ -0,0 +1,49 @@
+package com.spts.admin;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import com.spts.booking.Booking;
+import com.spts.helper.UpdateRoomAvailability;
+import com.spts.interfaces.ICheckOut;
+import com.spts.signup.User;
+
+@Component
+public class CheckoutImpl implements ICheckOut{
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private UpdateRoomAvailability available;
+
+	@Override
+	public int checkoutRooms(int bookingId) {
+		int code = -1;
+		code = changeBookingStatus(bookingId);
+		return code;
+
+	}
+	public int changeBookingStatus(int bookingId) {
+
+		String getBooking = "select * from booking where booking_id = ?";
+		int code = -1;
+
+		List<Booking> bookinglist = jdbcTemplate.query(getBooking, BeanPropertyRowMapper.newInstance(Booking.class),bookingId);
+
+		if(bookinglist.isEmpty())
+			return 1111;
+
+		String updateBookingQuery = "UPDATE booking SET booking_status = ? where booking_id = ?";
+
+		code = jdbcTemplate.update(updateBookingQuery,"CheckedOut",bookingId);
+
+		if(code != 1)
+			return code;
+		code = available.updateRooms(bookinglist.get(0),"CheckOut");
+		return code;
+	}
+}
